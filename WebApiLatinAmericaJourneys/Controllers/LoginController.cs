2d01d52e0b818c2782e3ab2e9f282f2e76751800
Bari David Gloria Garcia <bdavid2290@gmail.com>
2020-02-12 15:12:21 -0500

    Token

--- WebApiLatinAmericaJourneys/Controllers/LoginController.cs
+++ WebApiLatinAmericaJourneys/Controllers/LoginController.cs
@@ -52,7 +52,8 @@ namespace WebApiLatinAmericaJourneys.Controllers
                 lstToken.Add(item: fToken);
 
 
-                return Ok(lstToken);
+
+                return Ok(lstToken.FirstOrDefault());
             }
             else
             {
